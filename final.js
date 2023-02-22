1c07ccdd6c0eb8a8c522ab8e229fcf7baf685242
frosty-cell <119990197+frosty-cell@users.noreply.github.com>
2023-02-22 14:59:25 +0700

    Add files via upload

---
+++ final.js
@@ -0,0 +1,20 @@
+var inputBox = document.getElementById('inputBox')
+var AddButton = document.getElementById('AddButton')
+var table = document.getElementById("myTable");
+
+function inputBox(){
+    if(input == null){
+        alert(ข้อมูลไม่ครบ)
+    }
+}
+
+function AddButtonn() {
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = " " + ;
+    cell2.innerHTML = "NEW CELL2";
+    if(input == null){
+        alert(ข้อมูลไม่ครบ)
+    }
+  }
